08a09352d67891cb63895560eaee8618b0b96c23
Lars Greiss <kufikugel@googlemail.com>
2013-05-05 20:49:43 +0200

    Tuna: overwork liblights
    This commit does several things:
    
    - fix sometimes wonky notification LED
    - enable battery LED
    - battery LED can be turned off and on, as well color change
      for low, normal, full loaded state
    - abillity to enable blink on low battery
    
    cheers
    
    Change-Id: I3e9ea25aef48bba80a57517630db09679371b460

--- device.mk
+++ device.mk
@@ -101,6 +101,14 @@ PRODUCT_PACKAGES += \
         Nfc \
         Tag
 
+# LED brightness property
+PRODUCT_PROPERTY_OVERRIDES += \
+        persist.sys.led-brightness=31
+
+# Charging LED property
+PRODUCT_PROPERTY_OVERRIDES += \
+        persist.sys.enable-charging-led=1
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
         LiveWallpapers \
