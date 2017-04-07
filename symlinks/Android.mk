v01dz777/android_device_samsung_tuna

Fix NFC firmware lib loading, clean-up board symlink mess.
Nougat doesn't allow the NFC firmware .so to be loaded from /vendor/firmware.
Building the .so to /vendor/lib and symlinking to it works fine though.

Also clean up the strange rootdir Android.mk

Remove the archaic BOARD_CREATE_TUNA_HDCP_KEYS_SYMLINK flag.

symlinks/Android.mk
@@ -1,0 +1,40 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := tuna_hdcp_keys
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): TARGET := /factory/hdcp.keys
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/firmware/hdcp.keys
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(TARGET) $(SYMLINK)
+	$(hide) touch $@
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libpn544_fw.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): TARGET := /system/vendor/firmware/libpn544_fw.so
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/firmware/libpn544_fw.so
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(TARGET) $(SYMLINK)
+	$(hide) touch $@

SlimRoms/device_samsung_tuna

Change-Id: I51a37ab2038e4b54c2bb45fda1ad10c668ccd82a

symlinks/Android.mk
@@ -1,0 +1,40 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := tuna_hdcp_keys
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): TARGET := /factory/hdcp.keys
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/firmware/hdcp.keys
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(TARGET) $(SYMLINK)
+	$(hide) touch $@
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libpn544_fw.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): TARGET := /system/vendor/lib/libpn544_fw.so
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/firmware/libpn544_fw.so
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(TARGET) $(SYMLINK)
+	$(hide) touch $@