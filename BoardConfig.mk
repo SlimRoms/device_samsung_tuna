4e5d09527cf5b9fe48cb7d2b33f86e8478c91b0c
Andreas Blaesius <skate4life@gmx.de>
2017-07-11 20:20:23 +0200

    tuna: switch to common omap4 kernel
    Change-Id: Ib164ea8e681502a26abd086cc2868919bc39cef0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ TARGET_OTA_ASSERT_DEVICE := tuna,maguro,toro,toroplus
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME)
 TARGET_KERNEL_CONFIG := tuna_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/tuna
+TARGET_KERNEL_SOURCE := kernel/ti/omap4
 KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/arm/arm-eabi-4.7/bin"
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
